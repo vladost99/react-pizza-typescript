5f9b107aec3d3d787d2ca16f4376614481307fc0
Vlad <vladsa000@gmail.com>
2022-08-29 23:43:05 +0300

    added typescript for hook useClickOutside and in search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -21,7 +21,7 @@ const Search: React.FC = () => {
     []
   );
 
-  const onChangeInput = (event: any) => {
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
     updateSearchValue(event.target.value);
   }
