8b43c6173db7f7309d44c267084f6b144a42fc63
Vlad <vladsa000@gmail.com>
2022-09-04 21:50:56 +0300

    remove function getPizza in API

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,8 @@
+import { useDispatch } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import filters from 'redux/filter/slice';
 import cart from 'redux/cart/slice';
 import pizza from 'redux/pizza/slice';
-import { useDispatch } from 'react-redux';
 
 export const store = configureStore({
   reducer: {filters, cart, pizza},
